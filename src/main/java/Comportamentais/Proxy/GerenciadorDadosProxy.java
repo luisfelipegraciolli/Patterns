eef4dc28363fefe262d51f54dee16e124a8b8c4a
Luis Felipe Graciolli <shiwa101@proton.me>
2025-12-09 18:19:12 -0300

    Resolve problema com o Proxy

---
+++ src/main/java/Comportamentais/Proxy/GerenciadorDadosProxy.java
@@ -0,0 +1,24 @@
+package Comportamentais.Proxy;
+
+public class GerenciadorDadosProxy implements IGerenciadorDados{
+    GerenciadorDadosConfidenciais realSubject;
+    String nivelUsuario;
+    public GerenciadorDadosProxy(String nivelUsuario){
+        this.realSubject = new GerenciadorDadosConfidenciais();
+        this.nivelUsuario = nivelUsuario;
+    }
+
+    @Override
+    public void exibirDados(int id) {
+        realSubject.exibirDados(id);
+    }
+
+    @Override
+    public void excluirDados(int id) {
+        if(nivelUsuario.equals("ADMIN")){
+            realSubject.excluirDados(id);
+        }else{
+            System.out.println("ERRO: USER IS NOT ADMIN");
+        }
+    }
+}
