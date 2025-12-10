622f77fea0da973cbed2897708f3424120d2b079
Luis Felipe Graciolli <shiwa101@proton.me>
2025-12-10 04:46:13 -0300

    Resolve problema usando Facade

--- src/main/java/Comportamentais/Facade/CarrinhoComprasCliente.java
+++ src/main/java/Comportamentais/Facade/CarrinhoComprasCliente.java
@@ -1,21 +1,15 @@
 package Comportamentais.Facade;
 
 public class CarrinhoComprasCliente {
-    public void finalizarCompra(String produtoId, double valor, String cliente) {
-        GerenciadorEstoque estoque = new GerenciadorEstoque();
-        ProcessadorPagamento pagamento = new ProcessadorPagamento();
-        ServicoNotificacao notificacao = new ServicoNotificacao();
+    public void executarCompra(String produtoId, double valor, String cliente) {
+        ProcessoCompraFacade facade = new ProcessoCompraFacade();
 
-        if (estoque.verificarDisponibilidade(produtoId)) {
-            if (pagamento.processarTransacao(valor)) {
-                estoque.reservar(produtoId);
-                notificacao.enviarConfirmacao(cliente);
-                System.out.println("Sucesso! Compra finalizada para o cliente: " + cliente);
-            } else {
-                System.out.println("Falha: Processamento de pagamento negado.");
-            }
+        boolean sucesso = facade.finalizarCompra("101", 12.99, "Julia Temoteo"); // :p
+
+        if(sucesso){
+            System.out.println("Status final: Pedido Concluído.");
         } else {
-            System.out.println("Falha: Produto indisponível em estoque.");
+            System.out.println("Status final: Pedido Cancelado.");
         }
     }
 }
---
+++ src/main/java/Comportamentais/Facade/ProcessoCompraFacade.java
@@ -0,0 +1,30 @@
+package Comportamentais.Facade;
+
+public class ProcessoCompraFacade {
+    private GerenciadorEstoque estoque;
+    private ProcessadorPagamento pagamento;
+    private ServicoNotificacao notificacao;
+
+    public ProcessoCompraFacade(){
+        this.estoque = new GerenciadorEstoque();
+        this.pagamento = new ProcessadorPagamento();
+        this.notificacao = new ServicoNotificacao();
+    }
+
+    public boolean finalizarCompra(String produtoId, double valor, String cliente){
+        if (estoque.verificarDisponibilidade(produtoId)) {
+            if (pagamento.processarTransacao(valor)) {
+                estoque.reservar(produtoId);
+                notificacao.enviarConfirmacao(cliente);
+                System.out.println("Sucesso! Compra finalizada para o cliente: " + cliente);
+                return true;
+            } else {
+                System.out.println("Falha: Processamento de pagamento negado.");
+                return false;
+            }
+        } else {
+            System.out.println("Falha: Produto indisponível em estoque.");
+            return false;
+        }
+    }
+}
